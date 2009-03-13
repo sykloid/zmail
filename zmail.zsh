8fe3c779b5989bc2519267dfa3cf1e5060c307c7
sykora <sykora@lucentbeing.com>
2009-03-13 16:30:15 +0530

    Finished display function, with color.

--- zmail.zsh
+++ zmail.zsh
@@ -9,17 +9,19 @@
 # Declare arrays to hold the names of mailboxes with new mail, and the
 # corresponding count of new mail.
 typeset -TU MAILBOXES mailboxes
-typeset -TU MAILCOUNTER mailcount
+typeset -TU MAILCOUNTS mailcounts
+integer MAILTOTAL
 
 export MAILBOXES
-export MAILCOUNTER
+export MAILCOUNTS
+export MAILTOTAL
 
 typeset MAILDIR=~/.mail
 typeset MAILIGNORE=${MAILDIR}/ignore
 
 zmail_count() {
     MAILBOXES=
-    MAILCOUNTER=
+    MAILCOUNTS=
 
     typeset -U boxes
     typeset -U ignore
@@ -40,16 +42,52 @@ zmail_count() {
         done
         if (( box_count > 0 )); then
             mailboxes=($mailboxes $i:t)
-            mailcount=($mailcount $box_count)
+            mailcounts=($mailcounts $box_count)
         fi
     done
 
     export MAILBOXES
-    export MAILCOUNTER
+    export MAILCOUNTS
+    export MAILTOTAL=$total_count
 }
 
 zmail_display() {
+    if (( MAILTOTAL == 0 )); return
+    mail_header_color=$bold_color$fg256[15]
+
+    mailbox_name_color=$bold_color$fg256[214]
+    mailbox_count_color=$bold_color
+    mailbox_text_color=$reset_color
+
+    mail_bar_color=$reset_color
+
+    mail_total_count_color=$bold_color
+    mail_total_text_color=$reset_color
+    mail_total_color=$reset_color
+
+    printf "${mail_header_color}Zsh Mailman: ${reset_color}\n"
+    printf "${mail_bar_color}--------------------------------------------------${reset_color}\n"
+
     for i in {1..${#mailboxes}}; do
-        print $mailboxes[$i] $mailcount[$i]
+        # print $mailboxes[$i] $mailcounts[$i]
+        printf "${mailbox_count_color}%7d${mailbox_text_color}" $mailcounts[i]
+
+        if (( mailcounts[i] > 1 )); then
+            printf " new mails in "
+        else
+            printf " new mail in "
+        fi
+
+        printf "${mailbox_name_color}$mailboxes[i]${reset_color}.\n"
     done
+    printf "${mail_bar_color}--------------------------------------------------${reset_color}\n"
+    printf "${mail_total_count_color}%7d${mail_total_text_color}" $MAILTOTAL
+
+    if (( MAILTOTAL > 1 )); then
+        printf " new mails in "
+    else
+        printf " new mail in "
+    fi
+
+    printf "${mail_total_color}total.${reset_color}\n"
 }
